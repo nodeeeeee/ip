d945e0f4cb2faa78f9979abe2febea45a49fdec2
nodeeeeee <zhangkai.nodeee@gmail.com>
2026-01-28 17:07:26 +0800

    [Add Feature]: Level 8

--- src/main/java/Task/DeadlineTask.java
+++ src/main/java/Task/DeadlineTask.java
@@ -2,19 +2,42 @@ package Task;
 
 import SenpaiException.SenpaiException;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class DeadlineTask extends Task {
     public DeadlineTask(String taskDescription) throws SenpaiException{
-        super(taskDescription.split(" /by", 2)[0]);
-        if (taskDescription.split(" /by", 2).length == 1) {
+        super(taskDescription.split(" /by ", 2)[0]);
+        if (taskDescription.split(" /by ", 2).length == 1) {
             throw new SenpaiException("Mulimomuli!!! The due of a ddl cannot be empty.");
         }
-        due = taskDescription.split(" /by", 2)[1];
+        String due_str = taskDescription.split(" /by ", 2)[1];
+
+        try {
+            if (!due_str.contains("-")) {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+                due = LocalDate.parse(due_str, formatter);
+            } else {
+                due = LocalDate.parse(due_str);
+
+            }
+        } catch (DateTimeParseException e) {
+            System.out.println("date time format should be yyyy-mm-dd");
+            return ;
+        }
     }
 
     @Override
     public String getRep() {
-        return "D | "  + getStatus() + " | " + getTaskName() + " | /by" + due;
+        return "D | "  + getStatus() + " | " + getTaskName() + " | /by " + due.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
     }
 
-    final private String due;
+    public LocalDate getDue() {
+        return due;
+    }
+
+    private LocalDate due;
+
 }
--- src/main/java/Task/EventTask.java
+++ src/main/java/Task/EventTask.java
@@ -1,25 +1,49 @@
 package Task;
 
 import SenpaiException.SenpaiException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 
 public class EventTask extends Task {
     public EventTask(String taskDescription) throws SenpaiException {
-        super(taskDescription.split(" /from", 2)[0]);
-        if (taskDescription.split(" /from", 2).length == 1) {
+        super(taskDescription.split(" /from ", 2)[0]);
+        if (taskDescription.split(" /from ", 2).length == 1) {
             throw new SenpaiException("Mulimomuli!!! The starting time of a ddl cannot be empty.");
-        } else if (taskDescription.split(" /from", 2)[1].split(" /to", 2).length == 1) {
+        } else if (taskDescription.split(" /from ", 2)[1].split(" /to ", 2).length == 1) {
             throw new SenpaiException("Mulimomuli!!! The end time of a ddl cannot be empty.");
         }
-        from = taskDescription.split(" /from", 2)[1].split(" /to", 2)[0];
-        to = taskDescription.split(" /from", 2)[1].split(" /to", 2)[1];
+        String from_str = taskDescription.split(" /from ", 2)[1].split(" /to ", 2)[0];
+        String to_str = taskDescription.split(" /from ", 2)[1].split(" /to ", 2)[1];
+        if (!from_str.contains("-")) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+            from = LocalDate.parse(from_str, formatter);
+        } else {
+            from = LocalDate.parse(from_str);
+        }
+        if (!from_str.contains("-")) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+            to = LocalDate.parse(to_str, formatter);
+        } else {
+            to = LocalDate.parse(to_str);
+        }
 
     }
 
     @Override
     public String getRep() {
-        return "E | " + getStatus() + " | " + getTaskName() + " | /from" + from + " /to" + to;
+        return "E | " + getStatus() + " | " + getTaskName() + " | /from " + from.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + " /to " + to.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
     }
 
-    final private String from;
-    final private String to;
+    public LocalDate getFrom() {
+        return from;
+    }
+
+    public LocalDate getTo() {
+        return to;
+    }
+
+    private LocalDate from;
+    private LocalDate to;
 }
