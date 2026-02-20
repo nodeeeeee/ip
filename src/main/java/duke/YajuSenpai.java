11931484f43b681a60efadfcecbd8457fd70f987
nodeeeeee <zhangkai.nodeee@gmail.com>
2026-02-20 17:50:03 +0800

    [Add Feature]: Personality

--- src/main/java/duke/YajuSenpai.java
+++ src/main/java/duke/YajuSenpai.java
@@ -1,5 +1,7 @@
 package duke;
 
+import java.util.Random;
+
 import duke.senpaiexception.SenpaiException;
 
 /**
@@ -10,6 +12,7 @@ public class YajuSenpai {
     private TaskList tasks;
     private Storage storage;
     private String commandType = "";
+    private final Random rng = new Random();
     /**
      * Initialization.
      *
@@ -62,9 +65,9 @@ public class YajuSenpai {
             }
             String response = c.execute(tasks);
             commandType = c.getType().toString();
-            return response;
+            return addPersonality(response, commandType, false);
         } catch (SenpaiException e) {
-            return e.getResponse().print();
+            return addPersonality(e.getResponse().print(), "", true);
         }
     }
 
@@ -75,6 +78,77 @@ public class YajuSenpai {
      */
     public String getCommandType() {
         return commandType;
+    }
+
+    private String addPersonality(String response, String type, boolean isError) {
+        String prefix = pickPrefix(type, isError);
+        return prefix + response;
+    }
+
+    private String pickPrefix(String type, boolean isError) {
+        String[] errorPrefixes = new String[] {
+                "YajuSenpai: Oi oi, slow down. ",
+                "YajuSenpai: Hmm, that doesn't look right. ",
+                "YajuSenpai: Try that again, onegai. "
+        };
+        String[] addPrefixes = new String[] {
+                "YajuSenpai: Noted. ",
+                "YajuSenpai: Ryoukai. ",
+                "YajuSenpai: Done and dusted. "
+        };
+        String[] markPrefixes = new String[] {
+                "YajuSenpai: Nice work. ",
+                "YajuSenpai: Good job. ",
+                "YajuSenpai: Sasuga. "
+        };
+        String[] deletePrefixes = new String[] {
+                "YajuSenpai: Clean slate. ",
+                "YajuSenpai: Poof, gone. ",
+                "YajuSenpai: Vanished. "
+        };
+        String[] findPrefixes = new String[] {
+                "YajuSenpai: Look what I found. ",
+                "YajuSenpai: Here you go. ",
+                "YajuSenpai: I sniffed these out. "
+        };
+        String[] listPrefixes = new String[] {
+                "YajuSenpai: Your lineup, boss. ",
+                "YajuSenpai: Task parade incoming. ",
+                "YajuSenpai: The list, as you wish. "
+        };
+        String[] freePrefixes = new String[] {
+                "YajuSenpai: Scanning for gaps. ",
+                "YajuSenpai: Let me check your schedule. ",
+                "YajuSenpai: Free time hunt, go. "
+        };
+        String[] neutralPrefixes = new String[] {
+                "YajuSenpai: ",
+                "YajuSenpai: Roger. ",
+                "YajuSenpai: Mm-hmm. "
+        };
+
+        if (isError) {
+            return errorPrefixes[rng.nextInt(errorPrefixes.length)];
+        }
+        if ("E".equals(type) || "T".equals(type) || "D".equals(type)) {
+            return addPrefixes[rng.nextInt(addPrefixes.length)];
+        }
+        if ("mark".equals(type)) {
+            return markPrefixes[rng.nextInt(markPrefixes.length)];
+        }
+        if ("delete".equals(type)) {
+            return deletePrefixes[rng.nextInt(deletePrefixes.length)];
+        }
+        if ("find".equals(type)) {
+            return findPrefixes[rng.nextInt(findPrefixes.length)];
+        }
+        if ("list".equals(type)) {
+            return listPrefixes[rng.nextInt(listPrefixes.length)];
+        }
+        if ("free".equals(type)) {
+            return freePrefixes[rng.nextInt(freePrefixes.length)];
+        }
+        return neutralPrefixes[rng.nextInt(neutralPrefixes.length)];
     }
 
 
